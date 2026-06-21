a0b3cacde0c2a60c42ba4189372676821849ac31
Puja Agarwal <45425287+pujaagarwal5263@users.noreply.github.com>
2026-06-22 01:10:27 +0530

    Update cors.js

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -1,9 +1,7 @@
 const cors = require('cors');
 
 const corsOptions = {
-  origin: process.env.FRONTEND_URL === '*' 
-    ? '*' 
-    : 'https://rad-puffpuff-2be048.netlify.app',
+  origin: 'https://rad-puffpuff-2be048.netlify.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
